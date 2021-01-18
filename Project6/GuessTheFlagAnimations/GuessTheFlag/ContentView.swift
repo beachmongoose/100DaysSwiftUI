45887ff834f7c48704fee9cf496ec8e610ea959a
Maggie Maldjian <maggiemaldjian@gmail.com>
2021-01-18 14:41:26 -0500

    adding Project6 challenges

---
+++ Project6/GuessTheFlagAnimations/GuessTheFlag/ContentView.swift
@@ -0,0 +1,110 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Maggie Maldjian on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+  @State private var correctAnswer = Int.random(in: 0...2)
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  @State private var userScore = 0
+  @State private var round = 1
+  @State var makeSpin: Double = 0
+  @State var makeFade: Double = 1
+  @State var makeShake = 0
+  var body: some View {
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
+      VStack(spacing: 30) {
+        VStack {
+          Text("Tap the flag of")
+            .foregroundColor(.white)
+          Text(countries[correctAnswer])
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundColor(.white)
+        }
+        ForEach(0..<3) { number in
+          let isAnswer = (number == correctAnswer)
+          Button(action: {
+            withAnimation {
+              if isAnswer { makeSpin += 360
+                makeFade -= 0.25
+              } else {
+                makeShake += 1
+              }
+            }
+            self.flagTapped(number)
+          }) {
+            FlagImage(image: self.countries[number])
+              .rotation3DEffect(.degrees((isAnswer) ? makeSpin : 0), axis: (x: 0, y: 1, z: 0))
+              .opacity((isAnswer) ? 1 : makeFade)
+              .modifier(Shake(animatableData: CGFloat(makeShake)))
+              
+          }
+        }
+        Text("Score: \(userScore)")
+          .foregroundColor(.white)
+          .fontWeight(.black)
+        Spacer()
+      }
+      .alert(isPresented: $showingScore) {
+        Alert(title: Text(scoreTitle), message: Text("Your score is \(userScore)"), dismissButton: .default(Text((round < 10) ? "Continue" : "New Game")) {
+          self.askQuestion()
+        })
+      }
+    }
+  }
+
+  func flagTapped(_ number: Int) {
+    let isCorrect: Bool = number == correctAnswer
+    if isCorrect { userScore += 1 }
+    scoreTitle = (isCorrect) ? "Correct" : "Wrong! That is the flag of \(countries[number])."
+    showingScore = true
+  }
+
+  func askQuestion() {
+    makeFade = 1
+    makeShake = 0
+    if round == 10 {
+      round = 0
+      userScore = 0
+    } else { round += 1 }
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+  }
+}
+
+struct FlagImage: View {
+    let image: String
+    var body: some View {
+      Image(image)
+        .renderingMode(.original)
+        .clipShape(Capsule())
+        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+        .shadow(color: .black, radius: 2)
+    }
+  }
+
+// modified from objc.io/blog/2019/10/01/swiftui-shake-animation/
+struct Shake: GeometryEffect {
+  var animatableData: CGFloat
+  
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            10 * sin(animatableData * .pi * CGFloat(3)),
+            y: 0))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
